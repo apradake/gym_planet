d65c4c46848463a0b3346573b588742428ca303b
Aniket Radake <radake_a@India.XoriantCorp.com>
2021-12-04 16:39:28 +0530

    init

---
+++ src/main/java/com/gym/planet/Constants.java
@@ -0,0 +1,14 @@
+package com.gym.planet;
+
+public class Constants
+{
+
+	
+	private final static String con_username="planet";
+	private final static String con_password="planet";
+	
+	
+	
+	
+	
+}
--- src/main/java/com/gym/planet/MainController.java
+++ src/main/java/com/gym/planet/MainController.java
@@ -1,8 +1,11 @@
 package com.gym.planet;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import com.gym.planet.Constants;
 
 @Controller
 public class MainController
@@ -12,5 +15,27 @@ public class MainController
 	{
 		return "login.html";
 	}
+
+	
+	  @RequestMapping("/verifyuser")
+	  public ModelAndView verifyUserCredential(@RequestParam("username") String un, @RequestParam ("password") String pwd)
+	  {
+		  
+		  ModelAndView mv= new ModelAndView();
+		//  System.out.println(un);
+		//  System.out.println(pwd);
+		  if (un.equalsIgnoreCase("planet") && pwd.equalsIgnoreCase("planet"))
+		  {
+			  mv.setViewName("Homepage.html");
+		  }
+		  else
+		  {
+			  mv.setViewName("Error.html");
+		  }
+	  
+		  return mv;
+	  }
+	 
+	
 	
 }
